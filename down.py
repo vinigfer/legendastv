fe6e685df79359a864a66bc47dc74b8feec73d19
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2019-07-24 23:49:57 -0300

    style: Black

--- down.py
+++ down.py
@@ -4,22 +4,11 @@ from zipfile import ZipFile
 
 
 class LegendasTV(object):
-    URL_BUSCA = 'http://legendas.tv/legenda/busca/%s/1'
-    URL_DOWNLOAD = 'http://legendas.tv/downloadarquivo/%s'
-    URL_LOGIN = 'http://legendas.tv/login'
-    LEGENDA_LANG = {
-        'pt-br': 1,
-        'pt':    10,
-        'en':    2,
-        'es':    3,
-        'other': 100,
-        'all':   99,
-    }
-    LEGENDA_TIPO = {
-        'release': 1,
-        'filme':   2,
-        'usuario': 3,
-    }
+    URL_BUSCA = "http://legendas.tv/legenda/busca/%s/1"
+    URL_DOWNLOAD = "http://legendas.tv/downloadarquivo/%s"
+    URL_LOGIN = "http://legendas.tv/login"
+    LEGENDA_LANG = {"pt-br": 1, "pt": 10, "en": 2, "es": 3, "other": 100, "all": 99}
+    LEGENDA_TIPO = {"release": 1, "filme": 2, "usuario": 3}
 
     def __init__(self, usuario, senha):
         self.usuario = usuario
@@ -29,43 +18,39 @@ class LegendasTV(object):
 
     def _login(self):
         session = requests.Session()
-        payload = {
-            'data[User][username]': self.usuario,
-            'data[User][password]': self.senha,
-            'data[lembrar]': 'on'
-        }
+        payload = {"data[User][username]": self.usuario, "data[User][password]": self.senha, "data[lembrar]": "on"}
         headers = {
-            'host': 'legendas.tv',
-            'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'accept-language': 'en-US,en;q=0.5',
-            'accept-encoding': 'gzip, deflate',
-            'referer': 'http://legendas.tv/',
-            'connection': 'keep-alive',
-            'upgrade-insecure-requests': '1',
-            'content-type': 'application/x-www-form-urlencoded',
-            'content-length': '112',
+            "host": "legendas.tv",
+            "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "accept-language": "en-US,en;q=0.5",
+            "accept-encoding": "gzip, deflate",
+            "referer": "http://legendas.tv/",
+            "connection": "keep-alive",
+            "upgrade-insecure-requests": "1",
+            "content-type": "application/x-www-form-urlencoded",
+            "content-length": "112",
         }
         request = session.post(self.URL_LOGIN, data=payload, headers=headers)
         html_code = str(request._content)
 
         if self.usuario not in html_code:
-            print('Login failed')
+            print("Login failed")
             return False
         else:
             self.session = session
             return True
 
-    def _request(self, url, method='GET', data=None, headers=None):
+    def _request(self, url, method="GET", data=None, headers=None):
         if self.session:
-            if method == 'GET':
+            if method == "GET":
                 return self.session.get(url, stream=True, headers=headers)
-            if method == 'POST' and data:
+            if method == "POST" and data:
                 return self.session.post(url, data=data, headers=headers)
         else:
             pass  # raise exception
 
-    def search(self, series_name, episode_code, lang='pt-br', tipo='release'):
+    def search(self, series_name, episode_code, lang="pt-br", tipo="release"):
         q = series_name + " " + episode_code
         if not q:
             pass  # raise exception
@@ -76,15 +61,9 @@ class LegendasTV(object):
         if not tipo or not self.LEGENDA_TIPO.get(tipo):
             pass  # raise exception
 
-        busca = {
-            'txtLegenda': q,
-            'int_idioma': self.LEGENDA_LANG[lang],
-            'selTipo': self.LEGENDA_TIPO[tipo],
-        }
-        headers = {
-            'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-        }
-        request = self._request(self.URL_BUSCA % q, method='POST', data=busca, headers=headers)
+        busca = {"txtLegenda": q, "int_idioma": self.LEGENDA_LANG[lang], "selTipo": self.LEGENDA_TIPO[tipo]}
+        headers = {"user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"}
+        request = self._request(self.URL_BUSCA % q, method="POST", data=busca, headers=headers)
         if request:
             return self._parser((request.text).encode("utf-8"), series_name, episode_code)
         else:
@@ -103,15 +82,13 @@ class LegendasTV(object):
                     return url
 
     def download(self, url_legenda, file_absolute_path, video_name=None):
-        download_header = {
-            'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-        }
-        request = self._request(url_legenda, method='GET', headers=download_header)
+        download_header = {"user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"}
+        request = self._request(url_legenda, method="GET", headers=download_header)
         if request:
             file_absolute_path_with_extension = file_absolute_path + ".zip"
-            with open(file_absolute_path_with_extension, 'wb') as handle:
+            with open(file_absolute_path_with_extension, "wb") as handle:
                 handle.write(request.content)
 
-            with ZipFile(file_absolute_path_with_extension, 'r') as zip_file:
+            with ZipFile(file_absolute_path_with_extension, "r") as zip_file:
                 for name in zip_file.namelist():
-                    print('%s' % (name))
+                    print("%s" % (name))
